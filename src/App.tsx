ac2fcb814a8230404fa14de82fa1e7aaead18964
高毅 <1371834358@qq.com>
2024-06-03 15:21:11 +0800

    "feat avatar"

--- src/App.tsx
+++ src/App.tsx
@@ -21,6 +21,7 @@ function App() {
                     height: '100vh',
                     width: '100vw',
                 }}
+                assistantMeta={{avatar:'http://211.69.16.48:8888/src/assets/pdsu.jpg'}}
                 locale="en-US"
                  request={ async (messages) => {
                    const res =   await axios.post('/api',{
